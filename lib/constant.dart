526ca7baf8d36e93d5dae4fc699917029a5cd379
Anish Adhikari <141607712+aanish37@users.noreply.github.com>
2023-10-31 07:46:12 +0545

    Delete lib directory

--- lib/constant.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import './view/dashboard.dart';
-import 'view/contact_screen.dart';
-
-const pages = [Dashboard(), ContactScreen()];
-
-const backgroundColor = Colors.purple;
